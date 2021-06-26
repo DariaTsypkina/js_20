34998d0a08a06048354da96cb16835877c527b5e
DariaTsypkina <77797372+DariaTsypkina@users.noreply.github.com>
2021-06-26 21:39:10 +0300

    heroes gallery

---
+++ heroes/js/heroes.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    fetch("assets/json/heroes.json")
+    .then(response => response.json())
+    .then(response => {
+        let heroes = response.members;
+
+        document.getElementById("gallery").innerHTML = "";
+
+        for (let hero of heroes) {
+            hero.powers = hero.powers
+            document.getElementById("gallery").innerHTML += 
+            `<div class="gallery__card card" id="card">
+            <div class="img-container">
+                <img class="card__img" src="${hero.imgSRC}" alt="">
+            </div>
+            <div class="card__info">
+                    <h2 class="card__title">${hero.name}</h2>
+                    <ul class="card__list">
+                        <li class="card__item">Возраст: ${hero.age}</li>
+                        <li class="card__item">Имя: ${hero.alterEgo}</li>
+                        <li class="card__item">Суперсилы: ${hero.powers.join(",").replace(",", ", ")}</li>
+                        <li class="card__item">Род деятельности: гений, миллиардер, плейбой, филантроп</li>
+                        <li class="card__item">Друзья: Мстители</li>
+                    </ul>
+                    <div class="hero-grade">
+                        <label class="hero-grade__title" for="grade">Моя оценка:</label><br>
+                        <input class="hero-grade__input" type="number" min="0" max="5" id="heroGradeInput">
+                    </div>
+                </div>
+        </div>`;
+        console.log(hero.powers.join(",").replace(",", ", "));
+        }
+    });
+});
+
