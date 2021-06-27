de38a64006d11d4f477c79fafb7339e3fb9fb139
DariaTsypkina <77797372+DariaTsypkina@users.noreply.github.com>
2021-06-27 21:12:11 +0300

    +

--- heroes/js/heroes.js
+++ heroes/js/heroes.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     generateCards();
 });
 
+
 function generateCards() {
     fetch("assets/json/heroes.json")
     .then(response => response.json())
@@ -10,25 +11,25 @@ function generateCards() {
 
         document.getElementById("gallery").innerHTML = "";
 
-        for (let hero of heroes) {
+        for (let i = 0; i < heroes.length; i++) {
             document.getElementById("gallery").innerHTML += 
             `<div class="gallery__card card" id="card">
             <div class="img-container">
-                <img class="card__img" src="${hero.imgSRC}" alt="">
+                <img class="card__img" src="${heroes[i].imgSRC}" alt="">
             </div>
             <div class="card__info">
-                    <h2 class="card__title">${hero.name}</h2>
+                    <h2 class="card__title">${heroes[i].name}</h2>
                     <ul class="card__list">
-                        <li class="card__item"><span class="bold">Возраст: </span>${hero.age}</li>
-                        <li class="card__item"><span class="bold">Имя: </span>${hero.alterEgo}</li>
-                        <li class="card__item"><span class="bold">Суперсилы: </span>${hero.powers.join(", ")}</li>
-                        <li class="card__item"><span class="bold">Род деятельности: </span>${hero.activities.join(", ")}</li>
-                        <li class="card__item"><span class="bold">Друзья: </span>${hero.friends.join(", ")}</li>
-                        <li class="card__item"><span class="bold">Подробнее: </span>${hero.addInfo}</li>
+                        <li class="card__item"><span class="bold">Возраст: </span>${heroes[i].age}</li>
+                        <li class="card__item"><span class="bold">Имя: </span>${heroes[i].alterEgo}</li>
+                        <li class="card__item"><span class="bold">Суперсилы: </span>${heroes[i].powers.join(", ")}</li>
+                        <li class="card__item"><span class="bold">Род деятельности: </span>${heroes[i].activities.join(", ")}</li>
+                        <li class="card__item"><span class="bold">Друзья: </span>${heroes[i].friends.join(", ")}</li>
+                        <li class="card__item"><span class="bold">Подробнее: </span>${heroes[i].addInfo}</li>
                     </ul>
                     <div class="hero-grade">
                         <label class="hero-grade__title" for="grade">Моя оценка:</label><br>
-                        <input class="hero-grade__input" name="grade" type="number" min="0" max="5" step="0.5" onchange="setGrade()">
+                        <input class="hero-grade__input" name="grade" type="number" min="0" max="5" step="0.5" id="input-${i}" value="" onchange="setGrade()">
                         <i class="fas fa-star star"></i>
                     </div>
             </div>`;
@@ -47,12 +48,9 @@ function setGrade() {
             setStorage(grades);
             }
         }
-
 }
 
 function setStorage(grades) {
     localStorage.setItem('grades', grades);
 }
-
-
 
