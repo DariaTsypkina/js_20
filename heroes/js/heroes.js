f733850c8e015cca5d095523a6091275060fc6f4
DariaTsypkina <77797372+DariaTsypkina@users.noreply.github.com>
2021-06-27 15:54:26 +0300

    add grades to storage

--- heroes/js/heroes.js
+++ heroes/js/heroes.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    generateCards();
+});
+
+function generateCards() {
     fetch("assets/json/heroes.json")
     .then(response => response.json())
     .then(response => {
@@ -15,19 +19,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
             <div class="card__info">
                     <h2 class="card__title">${hero.name}</h2>
                     <ul class="card__list">
-                        <li class="card__item">Возраст: ${hero.age}</li>
-                        <li class="card__item">Имя: ${hero.alterEgo}</li>
-                        <li class="card__item">Суперсилы: ${hero.powers.join(", ")}</li>
-                        <li class="card__item">Род деятельности: ${hero.activities.join(", ")}</li>
-                        <li class="card__item">Друзья: ${hero.friends.join(", ")}</li>
+                        <li class="card__item"><span class="bold">Возраст: </span>${hero.age}</li>
+                        <li class="card__item"><span class="bold">Имя: </span>${hero.alterEgo}</li>
+                        <li class="card__item"><span class="bold">Суперсилы: </span>${hero.powers.join(", ")}</li>
+                        <li class="card__item"><span class="bold">Род деятельности: </span>${hero.activities.join(", ")}</li>
+                        <li class="card__item"><span class="bold">Друзья: </span>${hero.friends.join(", ")}</li>
+                        <li class="card__item"><span class="bold">Подробнее: </span>${hero.addInfo}</li>
                     </ul>
                     <div class="hero-grade">
                         <label class="hero-grade__title" for="grade">Моя оценка:</label><br>
-                        <input class="hero-grade__input" type="number" min="0" max="5" id="heroGradeInput">
+                        <input class="hero-grade__input" name="grade" type="number" min="0" max="5" step="0.5" onchange="setGrade()">
+                        <i class="fas fa-star star"></i>
                     </div>
-                </div>
-        </div>`;
+            </div>`;
         }
     });
-});
+}
+
+function setGrade() {
+    let inputs = document.querySelectorAll("input");
+    let grades = [];
+
+    for (let i of inputs) {
+        if (i.value < 0 || i.value > 5) return;
+        else {
+            grades.push(i.value);
+            setStorage(grades);
+            }
+        }
+}
+
+function setStorage(grades) {
+    localStorage.setItem('grades', grades);
+}
+
+
 
