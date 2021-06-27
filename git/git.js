d570d4346d4f5183d7467008c6668e0dff1de3fd
DariaTsypkina <77797372+DariaTsypkina@users.noreply.github.com>
2021-06-27 17:27:01 +0300

    git repos

--- git/git.js
+++ git/git.js
@@ -2,6 +2,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
     fetch('https://api.github.com/users/DariaTsypkina/repos')
         .then(response => response.json())
         .then(response => {
-            document.getElementById("container").innerHTML =""
+
+            for (let i = 0; i < response.length; i++) {
+
+                document.getElementById("container").innerHTML +=
+                `<div class="item">
+                <h2>Название проекта: ${response[i].name}</h2>
+                <ul>
+                    <li><a href="${response[i].html_url}">Ссылка на проект</a></li>
+                    <li>Язык: ${response[i].language}</li>
+                </ul>
+                </div>`;
+            }
         })
+        .catch(error => alert(error));
 });
